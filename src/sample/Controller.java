e01713b27648ff7a809de9a8e67300e7bda15af9
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-03-20 11:08:53 +0100

    Add actionEvent ang print out source when button clicked

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,13 +3,15 @@ package sample;
 import javafx.fxml.FXML;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Controller {
     @FXML
     private TextField nameField;
 
     @FXML
-    public void onButtonClicked() {
+    public void onButtonClicked(ActionEvent e) {
         System.out.println("Hello, " + nameField.getText());
+        System.out.println("The following button was pressed:" + e.getSource());
     }
 }
